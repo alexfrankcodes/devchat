21b7d37f87d099513162fc93184132f512fe4e1e
Alex Frank <alexfrankcodes@gmail.com>
2021-01-31 13:27:47 -0800

    Added basic socket.io connection

--- server/dist/index.js
+++ server/dist/index.js
@@ -6,9 +6,11 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const socket_io_1 = require("socket.io");
 const http_1 = require("http");
+const router_1 = __importDefault(require("./controllers/router"));
 const PORT = process.env.PORT || 5000;
 const app = express_1.default();
 const httpServer = http_1.createServer(app);
 const io = new socket_io_1.Server(httpServer);
+app.use(router_1.default);
 httpServer.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 //# sourceMappingURL=index.js.map
--- server/index.ts
+++ server/index.ts
@@ -1,11 +1,23 @@
 import express from "express";
-import { Server } from "socket.io";
+import { Server, Socket } from "socket.io";
 import { createServer } from "http";
+
+import router from "./controllers/router";
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 const httpServer = createServer(app);
 const io = new Server(httpServer);
+
+app.use(router);
+
+io.on("connection", (socket: Socket) => {
+  console.log("new connection");
+
+  socket.on("disconnect", () => {
+    console.log("disconnected");
+  });
+});
 
 httpServer.listen(PORT, () => console.log(`Server running on port ${PORT}`));
