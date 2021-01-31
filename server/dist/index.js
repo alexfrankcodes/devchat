88aa0106fb95dc6f145e434edb1ab2fb5b7935a1
Alex Frank <alexfrankcodes@gmail.com>
2021-01-31 13:18:48 -0800

    Set up backend express server w/ socket.io

--- server/dist/index.js
+++ server/dist/index.js
@@ -4,12 +4,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const socket_io_1 = require("socket.io");
+const http_1 = require("http");
+const PORT = process.env.PORT || 5000;
 const app = express_1.default();
-const port = 3000;
-app.get("/", (req, res) => {
-    res.send("The sedulous hyena ate the antelope!");
-});
-app.listen(port, () => {
-    return console.log(`server is listening on ${port}`);
-});
+const httpServer = http_1.createServer(app);
+const io = new socket_io_1.Server(httpServer);
+httpServer.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 //# sourceMappingURL=index.js.map
--- server/index.ts
+++ server/index.ts
@@ -1,10 +1,11 @@
 import express from "express";
+import { Server } from "socket.io";
+import { createServer } from "http";
+
+const PORT = process.env.PORT || 5000;
 
 const app = express();
-const port = 3000;
-app.get("/", (req, res) => {
-  res.send("The sedulous hyena ate the antelope!");
-});
-app.listen(port, () => {
-  return console.log(`server is listening on ${port}`);
-});
+const httpServer = createServer(app);
+const io = new Server(httpServer);
+
+httpServer.listen(PORT, () => console.log(`Server running on port ${PORT}`));
