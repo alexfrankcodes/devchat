8fbcd5c6ac4d726019294338b54aa303c31755af
Alex Frank <alexfrankcodes@gmail.com>
2021-01-30 16:47:45 -0800

    Created components and set up routing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,15 @@
-function App() {
-  return <div className="App"></div>;
-}
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import Join from "./components/Join";
+import Chat from "./components/Chat";
+
+const App = () => {
+  return (
+    <Router>
+      <Route path="/" exact component={Join} />
+      <Route path="/chat" exact component={Chat} />
+    </Router>
+  );
+};
 
 export default App;
