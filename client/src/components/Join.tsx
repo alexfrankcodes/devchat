529b3167c6a7cfb4985c7e0c20e039294044462d
Alex Frank <alexfrankcodes@gmail.com>
2021-02-01 17:06:04 -0800

    Implemented Join component

--- client/src/components/Join.tsx
+++ client/src/components/Join.tsx
@@ -1,5 +1,39 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
 const Join = () => {
-  return <h1>Join</h1>;
+  const [name, setName] = useState("");
+  const [room, setRoom] = useState("");
+
+  return (
+    <div className="joinOuterContainer">
+      <div className="joinInnerHeader">
+        <h1>Join</h1>
+        <div>
+          <input
+            placeholder="Username"
+            type="text"
+            onChange={(event) => setName(event.target.value)}
+          />
+        </div>
+        <div>
+          <input
+            placeholder="Room name"
+            type="text"
+            onChange={(event) => setRoom(event.target.value)}
+          />
+        </div>
+        <Link
+          onClick={(event) => (!name || !room ? event.preventDefault() : null)}
+          to={`/chat?name=${name}&room=${room}`}
+        >
+          <button className="button" type="submit">
+            Sign In
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Join;
