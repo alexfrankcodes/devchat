c4cfc68e0fe390aa2e740e3549e7b467739b152c
Alex Frank <alexfrankcodes@gmail.com>
2021-02-13 16:06:00 -0800

    Basic UserList styling + other styling fixes

--- client/src/components/Messages.tsx
+++ client/src/components/Messages.tsx
@@ -1,12 +1,14 @@
 import ScrollToBottom from "react-scroll-to-bottom";
 import Message from "./Message";
 
+import styles from "../styles/Messages.module.scss";
+
 const Messages: React.FC<{
   messages: Object[];
   name: string;
 }> = ({ messages, name }) => {
   return (
-    <ScrollToBottom>
+    <ScrollToBottom className={styles.container}>
       {messages.map((message: any, index: number) => (
         <div key={index}>
           <Message message={message} name={name} />
--- client/src/components/UserList.tsx
+++ client/src/components/UserList.tsx
@@ -1,6 +1,8 @@
+import styles from "../styles/UserList.module.scss";
+
 const UserList: React.FC<{ users: any }> = ({ users }) => {
   return (
-    <div>
+    <div className={styles.container}>
       <h2>Users</h2>
       {users.map((user: any) => (
         <p key={user.name}>{user.name}</p>
